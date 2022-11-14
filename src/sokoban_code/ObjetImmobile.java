06c2afef675e89756e59ce3259466ab4feef4394
Clément Burtin <clement.burtin@etu.uca.fr>
2022-11-14 10:08:34 +0100

    hey

---
+++ src/sokoban_code/ObjetImmobile.java
@@ -0,0 +1,36 @@
+package sokoban_code;
+
+public abstract class ObjetImmobile extends Objet{
+	//Attributs
+		private char m_type;
+		
+	//Constructeurs 
+	/**
+	 * Constructeur par defaut 
+	 */
+	public ObjetImmobile() {}
+	
+	/**
+	 * Construit un objet immobile selon les coordonnees x et y
+	 * @param x entier contenant la coordonnee x de l'objet
+	 * @param y entier contenant la coordonnee y de l'objet
+	 * @param t caractere contenant le type de l'objet immobile
+	 */	
+	public ObjetImmobile(int x, int y, char type) {
+		super(x,y);
+		m_type = type;
+	}
+	
+	//Methodes
+	/**
+	 * Donne acces au type de l'objet immobile ("#" : mur, "." : cible, "_" : vide)
+	 * @param caractere contenant le type de l'objet immobile
+	 */
+	public int getType() {return m_type;}
+	
+	/**
+	 * Permet de modifier le type de l'objet immobile
+	 * @param type : caractere contenant le nouveau type de l'objet immobile
+	 */
+	public void setType(char type){m_type=type;}
+}
---
+++ src/sokoban_code/ObjetMobile.java
@@ -0,0 +1,6 @@
+package sokoban_code;
+
+public abstract class ObjetMobile extends Objet{
+	//Attributs
+	private char m_type;
+}
